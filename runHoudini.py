33d48c1c6fcfa18f2aae2d04bcaf495dff02113c
KRYSKO Kiryha <KKO8@dsone.3ds.com>
2018-07-19 08:56:49 -0400

    Standard update

--- runHoudini.py
+++ runHoudini.py
@@ -4,40 +4,60 @@
 # Wrapper to run Houdini with custom environment
 import os, subprocess
 
-# DEFINE VARIABLES
+# DEFINE COMMON VARIABLES
 # Pipeline root folder
 rootPipeline = os.path.dirname(__file__).replace('\\','/')
 # Project root folder
 rootProject = os.path.dirname(os.path.dirname(rootPipeline)).replace('\\','/')
 # Houdini JOB root
 root3D = '{0}/PROD/3D'.format(rootProject)
+# HOUDINI_OTLSCAN_PATH
+pathHDA = ''
 # Houdini install dir
 PC = os.environ['COMPUTERNAME']
-houdini = 'C:/Program Files/Side Effects Software/Houdini 16.5.439/bin/houdinifx.exe'
-# houdini = 'C:/Program Files/Side Effects Software/Houdini 16.0.504.20/bin/houdinifx.exe'
+if PC == 'ANGEL':
+    houdini = 'C:/Program Files/Side Effects Software/Houdini 16.0.504.20/bin/houdinifx.exe'
+else:
+    houdini = 'C:/Program Files/Side Effects Software/Houdini 16.5.439/bin/houdinifx.exe'
 
-def getOTL():
+
+def getHDA():
     '''
-    Build HOUDINI_OTLSCAN_PATH value:
-    Get all subfolders of <root3D>/hda folder and combine to one string
+    Build HOUDINI_OTLSCAN_PATH env variable value:
+    Get all subfolders of HDA location dirs (hda and lib/materials) and combine to one string
+    Used by Houdini to search for HDA (Houdini Digital Assets)
     '''
 
-    pathOTL = ''
-    listPaths = os.walk('{0}/hda'.format(root3D)) # Get list of sub folders
+    def combinePaths(listPaths):
+        '''
+        Combine paths string from list of paths
+        :param listPaths: list of subfolders in processed folder
+        '''
+        global pathHDA
+        for folder in listPaths:
+            path = folder[0].replace('\\', '/')
+            if not 'backup' in path: # Exclude backup folders
+                pathHDA += '{};'.format(folder[0].replace('\\', '/'))
 
-    for folder in listPaths:
-        path = folder[0].replace('\\', '/')
-        if not 'backup' in path: # Exclude backup folders
-            pathOTL += '{};'.format(folder[0].replace('\\', '/'))
+    # Get list of sub folders
+    listPaths_HDA = os.walk('{0}/hda'.format(root3D)) # HDA
+    listPaths_MTL = os.walk('{0}/lib/MATERIALS'.format(root3D))  # LIBRARY
 
-    pathOTL = pathOTL + '&' # Add Houdini standard OTL paths
-    return pathOTL
+    # Combine paths to a string
+    combinePaths(listPaths_HDA)
+    combinePaths(listPaths_MTL)
+
+    # Add Houdini standard OTLs
+    global pathHDA
+    pathHDA = pathHDA + '&'
+
+    return pathHDA
 
 # SETUP PROJECT ENVIRONMENT
 # Root of houdini project
 os.environ['JOB'] = root3D
 # Houdini digital assets folder including sub folders
-os.environ['HOUDINI_OTLSCAN_PATH'] = getOTL()
+os.environ['HOUDINI_OTLSCAN_PATH'] = getHDA()
 # Houdini path
 os.environ['HOUDINI_PATH'] = '{}/MOTHER/houdini;&'.format(rootPipeline)
 # Icons
@@ -45,10 +65,8 @@ os.environ['HOUDINI_PATH'] = '{}/MOTHER/houdini;&'.format(rootPipeline)
 # Houdini current user pref folder in MyDocuments (win)
 # os.environ['home'] = '{}/Documents/houdiniUserPrefs'.format(os.path.expanduser("~"))
 
-
-
 # Run Houdini
 subprocess.Popen(houdini)
 
 # Prevent closing CMD window
-raw_input()
+# raw_input()
