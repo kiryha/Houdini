e3a6e93c0121979bb4351fce01bcc2a94316affb
KRYSKO Kiryha <KKO8@dsone.3ds.com>
2018-09-27 17:30:39 -0400

    Create Scene tool basic UI setup

---
+++ MOTHER/tools/createScene.py
@@ -0,0 +1,24 @@
+# 256 Pipeline tools
+# Create ANIMATION and RENDER scenes
+
+import hou
+import os
+from PySide2 import QtCore, QtUiTools, QtWidgets
+
+path_ui = os.environ['JOB'].replace('PROD/3D', 'PREP/PIPELINE/MOTHER/ui')
+ 
+class CreateScene(QtWidgets.QWidget):
+    def __init__(self):
+        super(CreateScene,self).__init__()
+        ui_file = "{}/createScene_Main.ui".format(path_ui)
+        self.ui = QtUiTools.QUiLoader().load(ui_file, parentWidget=self)
+        self.setParent(hou.ui.mainQtWindow(), QtCore.Qt.Window)
+        
+        self.ui.pushButton.clicked.connect(self.click)
+        
+    def click(self):
+        print 'A B C'
+
+def run():
+    CS = CreateScene()
+    CS.show()
