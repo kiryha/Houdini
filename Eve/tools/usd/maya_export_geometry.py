06f451df708899f5c6fc65c6b207e90872c2d798
Kiryha <kiryha@gmail.com>
2023-12-07 18:20:04 -0500

    refactor

--- Eve/tools/usd/maya_export_geometry.py
+++ Eve/tools/usd/maya_export_geometry.py
@@ -6,43 +6,68 @@ import random
 from pxr import Usd, UsdGeom
 import pymel.core as pm
 
-# Create USD stage
-usd_file_path = f'D:/maya_geometry_{random.randint(1, 1000)}.usda'
 
-# Create USD stage and root object
-stage = Usd.Stage.CreateNew(usd_file_path)
-root_xform = UsdGeom.Xform.Define(stage, '/')
+def get_geometry_data(mesh):
+    """
+    Get mesh geometry data
+    """
 
-for mesh in pm.ls(type='mesh'):
+    points = []  # World position coordinates (tuples) for each geometry point (point3f[] points)
+    face_vertex_counts = []  # Number of vertices in each geometry face (int[] faceVertexCounts)
+    face_vertex_indices = []  # List of geometry vertex indexes (int[] faceVertexIndices)
 
-    # Initialize geometry data
-    vertex_positions = []  # point3f[] points
-    face_vertex_counts = []  # int[] faceVertexCounts
-    face_vertex_indices = []  # int[] faceVertexIndices
-
-    # Create a USD Mesh primitive for the mesh object
-    usd_mesh = UsdGeom.Mesh.Define(stage, root_xform.GetPath().AppendChild(mesh.getParent().name()))
-
-    # Iterate over the faces and collect vertices and indices
+    # Get vertex data for each face
     vertex_index = 0
     for face in mesh.faces:
         vertex_indexes = []
         for vertex in face.getVertices():
             position = tuple(mesh.vtx[vertex].getPosition(space='world'))
-
-            # Add the vertex position to the list and its index to the face's vertex indexes
-            vertex_positions.append(position)
+            points.append(position)
             vertex_indexes.append(vertex_index)
             vertex_index += 1
 
         face_vertex_counts.append(len(vertex_indexes))
         face_vertex_indices.extend(vertex_indexes)
 
-    # Set the collected attributes for the USD Mesh
-    usd_mesh.GetPointsAttr().Set(vertex_positions)
-    usd_mesh.GetFaceVertexCountsAttr().Set(face_vertex_counts)
-    usd_mesh.GetFaceVertexIndicesAttr().Set(face_vertex_indices)
+    return points, face_vertex_counts, face_vertex_indices
 
-# Save the USD stage to the file
-stage.GetRootLayer().Save()
-print(f'>> {usd_file_path}')
+
+def process_geometry(stage, root_xform):
+    """
+    Iterate over all scene meshes and record them to the USD stage
+    """
+
+    for mesh in pm.ls(type='mesh'):
+
+        # Create a USD Mesh primitive for the mesh object
+        usd_mesh = UsdGeom.Mesh.Define(stage, root_xform.GetPath().AppendChild(mesh.getParent().name()))
+
+        # Get geometry data
+        points, face_vertex_counts, face_vertex_indices = get_geometry_data(mesh)
+
+        # Set the collected attributes for the USD Mesh
+        usd_mesh.GetPointsAttr().Set(points)
+        usd_mesh.GetFaceVertexCountsAttr().Set(face_vertex_counts)
+        usd_mesh.GetFaceVertexIndicesAttr().Set(face_vertex_indices)
+
+
+def export_geometry():
+    """
+    Create USD file and record geometry data
+    """
+
+    # Create USD stage
+    usd_file_path = f'D:/maya_geometry_{random.randint(1, 1000)}.usda'
+
+    # Create USD stage and root object
+    stage = Usd.Stage.CreateNew(usd_file_path)
+    root_xform = UsdGeom.Xform.Define(stage, '/')
+
+    process_geometry(stage, root_xform)
+
+    # Save the USD stage to the file
+    stage.GetRootLayer().Save()
+    print(f'>> {usd_file_path}')
+
+
+export_geometry()
