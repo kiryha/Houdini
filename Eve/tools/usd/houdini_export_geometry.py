d58c234839fce210eb4ad8d3054bf84ac3cf7f97
Kiryha <kiryha@gmail.com>
2023-12-08 13:40:56 -0500

    add normals to houdini usd exporter

--- Eve/tools/usd/houdini_export_geometry.py
+++ Eve/tools/usd/houdini_export_geometry.py
@@ -9,16 +9,35 @@ from pxr import Usd, UsdGeom, Gf
 import hou
 
 
+def setup_mesh(mesh, points, normals, face_vertex_counts, face_vertex_indices):
+    """
+    Setup mesh attributes in USD file
+    """
+
+    mesh.GetPointsAttr().Set(points)
+    mesh.GetFaceVertexCountsAttr().Set(face_vertex_counts)
+    mesh.GetFaceVertexIndicesAttr().Set(face_vertex_indices)
+
+    if normals:
+        mesh.CreateNormalsAttr().Set(normals)
+        mesh.SetNormalsInterpolation(UsdGeom.Tokens.faceVarying)
+
+    # Set orientation and subdivisionScheme
+    mesh.CreateOrientationAttr().Set(UsdGeom.Tokens.leftHanded)
+    mesh.CreateSubdivisionSchemeAttr().Set("none")
+
+
 def get_geometry_data(geometry):
     """
-    Get mesh geometry data
+    Get mesh geometry data including normals
     """
 
     points = []  # List of point positions (point3f[] points)
+    normals = []  # List of normals (normal3f[] normals)
     face_vertex_counts = []  # List of vertex count per face (int[] faceVertexCounts)
     face_vertex_indices = []  # List of vertex indices (int[] faceVertexIndices)
 
-    # Collect points
+    # Collect points and normals
     for point in geometry.points():
         position = point.position()
         points.append(Gf.Vec3f(position[0], position[1], position[2]))
@@ -27,14 +46,21 @@ def get_geometry_data(geometry):
     for primitive in geometry.prims():
         vertices = primitive.vertices()
         face_vertex_counts.append(len(vertices))
-        face_vertex_indices.extend([vertex.point().number() for vertex in vertices])
 
-    return points, face_vertex_counts, face_vertex_indices
+        for vertex in vertices:
+            face_vertex_indices.append(vertex.point().number())
+
+            # Get Normals data
+            if geometry.findVertexAttrib("N") is not None:
+                normal = vertex.attribValue("N")
+                normals.append(Gf.Vec3f(normal[0], normal[1], normal[2]))
+
+    return points, normals, face_vertex_counts, face_vertex_indices
 
 
 def export_geometry():
     """
-    Create and save a USD file with geometry from the first input.
+    Create and save a USD file with geometry and normals from the first input.
     """
 
     # Create a new USD stage
@@ -47,13 +73,12 @@ def export_geometry():
     input_node = node.inputs()[0]
     input_node_name = input_node.name()
 
-    points, face_vertex_counts, face_vertex_indices = get_geometry_data(geometry)
+    # Get Geometry data
+    points, normals, face_vertex_counts, face_vertex_indices = get_geometry_data(geometry)
 
-    # Create a USD Mesh primitive
+    # Create a USD Mesh primitive and set properties
     mesh = UsdGeom.Mesh.Define(stage, f'/Root/{input_node_name}')
-    mesh.GetPointsAttr().Set(points)
-    mesh.GetFaceVertexCountsAttr().Set(face_vertex_counts)
-    mesh.GetFaceVertexIndicesAttr().Set(face_vertex_indices)
+    setup_mesh(mesh, points, normals, face_vertex_counts, face_vertex_indices )
 
     # Save the stage
     stage.GetRootLayer().Save()
