76d03cbd07000ccea1e25ec4fc43d606c05e2ade
Kiryha <kiryha@gmail.com>
2025-03-18 16:22:19 -0400

    change Houdini EXE paths

--- Eve/project_manager.bat
+++ Eve/project_manager.bat
@@ -1,4 +1,4 @@
 set PYTHONPATH=%cd%\tools
 set EVE_ROOT=%cd%
 
-"C:\Users\kko8\AppData\Local\Programs\Python\Python310\python.exe" %cd%\tools\pm\project_manager.py
+"C:\Users\kko8\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\python.exe" %cd%\tools\pm\project_manager.py
