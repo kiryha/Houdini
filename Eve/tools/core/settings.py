f74e1b95beed73213020ee8793090e9643520cd3
Kiryha <kiryha@gmail.com>
2025-08-26 13:58:29 -0400

    remove space in Houdini install
    SideFX changed install path in 21 (removed space)

--- Eve/tools/core/settings.py
+++ Eve/tools/core/settings.py
@@ -8,7 +8,7 @@ SQL_FILE_PATH = '{0}/data/eve.db'
 PROJECTS = 'C:/Users/kko8/OneDrive/projects'
 # Houdini bin
 default_build = '19.5.368'
-HOUDINI = 'E:/Programs/Houdini {0}/bin/houdini.exe'
+HOUDINI = 'E:/Programs/Houdini{0}/bin/houdini.exe'
 # Unreal Engine bin
 UNREAL = 'E:/Programs/Epic Games/UE_5.5/Engine/Binaries/Win64/UnrealEditor.exe'
 # File formats
